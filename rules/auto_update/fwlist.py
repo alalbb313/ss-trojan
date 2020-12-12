2a37b7509c1f1bbf722adbaccc5d28d82c48ceb1
alalbb313 <313460137@qq.com>
2020-12-12 13:51:39 +0800

    fix fwlist.py

--- rules/auto_update/fwlist.py
+++ rules/auto_update/fwlist.py
@@ -7,7 +7,8 @@
 # Ref https://code.google.com/p/autoproxy-gfwlist/wiki/Rules    
  
 import sys
-import urllib2 
+import urllib.request as urllib2
+import urllib 
 import re
 import os
 import datetime
@@ -40,7 +41,7 @@ fs =  file(outfile, 'w')
 #fs.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
 #fs.write('#\n')
  
-print 'fetching list...'
+print("fetching list...")
 content = urllib2.urlopen(baseurl, timeout=15).read().decode('base64')
  
 # write the decoded content to file then read line by line
@@ -49,7 +50,7 @@ tfs.write(content)
 tfs.close()
 tfs = open(tmpfile, 'r')
  
-print 'page content fetched, analysis...'
+print("page content fetched, analysis...")
  
 # remember all blocked domains, in case of duplicate records
 domainlist = []
@@ -85,5 +86,5 @@ fs.close();
 #print 'restart dnsmasq...'
 #print os.popen('/etc/init.d/dnsmasq restart').read()
  
-print 'saving to file: ', outfile
-print 'done!'
+print("saving to file: ", outfile)
+print("done!")
