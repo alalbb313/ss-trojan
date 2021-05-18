ba65aa46b8a3238dab1371db7e0d3648b1fadef0
alalbb313 <yongchen.xiao@hotmail.com>
2021-05-18 22:18:55 +0800

    update

--- fancyss_arm384/config.json.js
+++ fancyss_arm384/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2021-05-15_00:24:19",
+"build_date":"2021-05-18_22:18:39",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"ec3230f6c16364b3ba71d0793e6e8522",
+"md5":"0cc1d28924a0b8522c0d309c5cb85ff3",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/alalbb313/ss-trojan/master/fancyss_arm384/shadowsocks_v1.1.0.tar.gz", 
 "title":"科学上网",
